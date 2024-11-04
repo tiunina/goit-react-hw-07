c1c1aada7b4385d69b8274b5313f2e3e6c9e1bdc
Alona Tiunina <163348743+tiunina@users.noreply.github.com>
2024-11-04 22:53:26 +0200

    second commit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,35 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from "redux-persist";
-import storage from "redux-persist/lib/storage";
-
 import { contactsReducer } from "./contacts/contactsSlice";
 import { filterReducer } from "./filter/filtersSlice";
 
-const contactsConfig = {
-  key: "contactsKey",
-  storage,
-  //   whitelist: ["users"], // blacklist: ["showProfilesList"]
-};
-
 export const store = configureStore({
   reducer: {
-    contacts: persistReducer(contactsConfig, contactsReducer),
+    contacts: contactsReducer,
     filter: filterReducer,
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
 });
-export const persistor = persistStore(store);
